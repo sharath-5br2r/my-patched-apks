a5da840b7f127097b00873c52866a275b4cb8cbf
Fahim Ahmed <fahim.ahmed1453@gmail.com>
2026-07-06 13:36:16 +0600

    fix: jq syntax error when referencing keys in ci_generate_configs.sh

--- .github/scripts/ci_generate_configs.sh
+++ .github/scripts/ci_generate_configs.sh
@@ -35,11 +35,12 @@ if [ "${TRIGGER_STABLE:-0}" = "1" ] || [ "${TRIGGER_APP_UPDATE:-0}" = "1" ] || [
   jq --slurpfile active active.stable.json --slurpfile activeApps active_apps.json '
     { "parallel-jobs": 1, "enable-module-update": true } as $force |
     ($force + . + $force) |
-    map_values(
-      if type == "object" then
-        . as $app |
+    with_entries(
+      if .value | type == "object" then
+        .key as $k |
+        .value as $app |
         (($app["patches-source"] // "ReVanced/revanced-patches") | ascii_downcase | gsub("[\"'\''\\n\\r\\t]"; " ") | split(" ") | map(select(. != ""))) as $srcs |
-        if (($srcs - $active[0]) != $srcs) or ($activeApps[0] | index(key)) then $app else ($app | .enabled = false) end
+        if (($srcs - $active[0]) != $srcs) or ($activeApps[0] | index($k)) then . else (.value.enabled = false) end
       else . end
     )
   ' config.stable.json > .github/configs/config.stable.updated.json
@@ -57,11 +58,12 @@ if [ "${TRIGGER_PRERELEASE:-0}" = "1" ]; then
   jq --slurpfile active active.prerelease.json --slurpfile activeApps active_apps.json '
     { "parallel-jobs": 1, "patches-version": "dev", "enable-module-update": false } as $force |
     ($force + . + $force) |
-    map_values(
-      if type == "object" then
-        . as $app |
+    with_entries(
+      if .value | type == "object" then
+        .key as $k |
+        .value as $app |
         (($app["patches-source"] // "ReVanced/revanced-patches") | ascii_downcase | gsub("[\"'\''\\n\\r\\t]"; " ") | split(" ") | map(select(. != ""))) as $srcs |
-        if (($srcs - $active[0]) != $srcs) or ($activeApps[0] | index(key)) then $app else ($app | .enabled = false) end
+        if (($srcs - $active[0]) != $srcs) or ($activeApps[0] | index($k)) then . else (.value.enabled = false) end
       else . end
     )
   ' config.dev.json > .github/configs/config.dev.updated.json
