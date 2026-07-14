b1e87234d1adb86b013bdd0f15b157878a9f51fc
Sharath <sharathsukash@gmail.com>
2026-07-14 09:11:41 +0530

    fix

--- scripts/configgen.sh
+++ scripts/configgen.sh
@@ -1,4 +1,2 @@
 yq -o=toml eval-all '. as $item ireduce ({}; . * $item)' configs/header.dev.toml configs/patches/*  > configs/config.dev.toml
 yq -o=toml eval-all '. as $item ireduce ({}; . * $item)' configs/header.stable.toml configs/patches/*  > configs/config.stable.toml
-yq -o=toml eval-all '. as $item ireduce ({}; . * $item)' configs/header.dev.toml configs/config.predl.toml  > configs/config.dev.predl.toml
-yq -o=toml eval-all '. as $item ireduce ({}; . * $item)' configs/header.stable.toml configs/config.predl.toml  > configs/config.stable.predl.toml
