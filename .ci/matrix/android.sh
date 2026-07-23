c3a0af8c0c68c0e39dcfb3bc6980acd24887649b
Sharath <sharathsukash@gmail.com>
2026-07-23 20:40:23 +0530

    Update android.sh

--- .ci/matrix/android.sh
+++ .ci/matrix/android.sh
@@ -2,13 +2,13 @@
 
 flavor() {
   cat <<-EOF
-		{"flavor": "$1"}
+		{"flavor": "$1", "arch": "$2" }
 	EOF
 }
 
 #standard=$(flavor standard)
-chromeos=$(flavor chromeos)
-optimized=$(flavor optimized)
+chromeos=$(flavor chromeos x86_64)
+optimized=$(flavor optimized arm64)
 #legacy=$(flavor legacy)
 
 # TODO: rename force_pgo to force_tag or something
